fe3dfbd8d701287a2ddb03edec1f4fec3127dec8
Timofey <timadol@yandex.ru>
2019-06-22 19:01:25 +0300

    Андрей, я тут запилил структурку для хранения значений, чтобы было оптимально по памяти и времени работы и функции, чтобы считывать из файла туда

--- main.c
+++ main.c
@@ -3,6 +3,83 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ERR_EOM -1
+#define ERR_EOF -2
+#define ERR_OUT_OF_BOUNDS -3
+
+#define BUFFSIZE 512
+typedef struct _LIST_
+{
+    /* data */
+    int buff[BUFFSIZE];
+    struct _LIST_ * next;
+} LIST;
+LIST Head;
+
+// Чтение строки из файла в связанный список
+// -----------------------------------------
+int LIST_readstr( int file )
+{
+    LIST *plist = &Head;
+    char c;
+    int n = 0;
+    int size = 0;
+
+    while( read(file, &c, 1) )
+    {
+        if( n < BUFFSIZE )
+            plist->buff[n++] = c;
+        else
+        {
+            if( !plist->next ) 
+            {
+                plist->next = malloc( sizeof( LIST ));
+                if( !plist->next)  
+                    return ERR_EOM;
+                plist->next->next = NULL;
+            }
+            plist = plist->next;
+            n = 0;
+        }
+        size++;
+        if( c == '\n' ) return size;
+    }
+    return ERR_EOF;
+}
+
+// Получение символа из связанного списка по его индексу
+// -----------------------------------------------------
+int LIST_getchr( unsigned int idx )
+{
+    LIST *plist = &Head;
+
+    while( idx >= BUFFSIZE )
+    {
+        if( plist->next )
+        {
+            plist = plist->next;
+            idx -= BUFFSIZE;
+        }
+        else return ERR_OUT_OF_BOUNDS;
+    }
+    return plist->buff[idx];    
+}
+
+// Высвобождение памяти, занятой связанным списком
+// -----------------------------------------------
+void LIST_destroy()
+{
+    LIST *next = Head.next->next;
+    LIST *elem = Head.next;
+    while (next)
+    {
+        free(elem);
+        elem = next;
+        next = next->next;
+    }
+    free(elem);
+}
+/*
 char *ft_getstr()
 {
     char *res;
@@ -28,8 +105,9 @@ char *ft_getstr()
     }
     return (res);
 }
+*/
 
-char    ft_getchar()
+char ft_getchar()
 {
     char c;
     if (read(0, &c, 1))
@@ -40,6 +118,21 @@ char    ft_getchar()
 int main(int argc, char *argv[])
 {
     char *str = ft_getstr();
+    char charset[3];
+    int len = strlen(str);
+
+    // инициализация списка
+    Head.next = NULL;
+
     printf("%s", str);
-    printf(", strlen = %d", strlen(str));
+    printf(", strlen = %d", len);
+    charset[2] = str[len - 1];
+    charset[1] = str[len - 2];
+    charset[0] = str[len - 3];
+    printf("\nEmpty - \"%c\"", charset[0]);
+    printf("\nObsticle - \"%c\"", charset[1]);
+    printf("\nFull - \"%c\"", charset[2]);
+
+    // освобождаем память
+
 }
